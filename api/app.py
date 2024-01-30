354574c55c8ec2923beb56850744c4e6b5b0a0c0
joelsousa21 <joelsousa21@outlook.com>
2024-01-30 08:46:08 -0300

    adiciona validacao na entrada de dados e formata com caracteres os numeros

--- api/app.py
+++ api/app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request
+from babel.numbers import format_currency
+import locale
 
 app = Flask(__name__)
 
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+def formatar_real(valor):
+    return format_currency(valor, 'BRL', locale='pt_BR')
+
 @app.route("/", methods=["GET", "POST"])
-def calcular_impostos():
+def index():
     if request.method == "POST":
         anexo = int(request.form.get("anexo"))
-        rbt12 = float(request.form.get("rbt12"))
-        receita_mensal = float(request.form.get("receita_mensal"))
+
+        rbt12 = float(request.form.get("rbt12").replace(".","").replace(",","."))
+        receita_mensal = float(request.form.get("receita_mensal").replace(".","").replace(",","."))
 
         resultado = calcular_impostos(anexo, rbt12, receita_mensal)
         return render_template("index.html", resultado=resultado)
@@ -22,8 +30,52 @@ def calcular_impostos(anexo, rbt12, receita_mensal):
         'receita_mensal': receita_mensal,
         'imposto_das': 0.0
     }
+# ANEXO I -  COMERCIO
+    if anexo == 1:
+        if rbt12 > 0 and rbt12 <= 180000:
+            aliq_efet = 0.04
+        elif rbt12 > 180000 and rbt12 <= 360000:
+            aliq_efet = (rbt12 * 0.073 - 5940) / rbt12
+        elif rbt12 > 360000 and rbt12 <= 720000:
+            aliq_efet = (rbt12 * 0.095 - 13860) / rbt12
+        elif rbt12 > 720000 and rbt12 <= 1800000:
+            aliq_efet = (rbt12 * 0.107 - 22500) / rbt12
+        elif rbt12 > 1800000 and rbt12 <= 3600000:
+            aliq_efet = (rbt12 * 0.143 - 87300) / rbt12
+        elif rbt12 > 3600000 and rbt12 <= 4800000:
+            calc_iss = ((rbt12 * 0.19 - 378000) / rbt12) * 0.335
+            iss_excedente = calc_iss * receita_mensal
+            aliq_efet = ((rbt12 * 0.33 - 648000) / rbt12) + calc_iss
+        resultado['imposto_das'] = receita_mensal * aliq_efet
+        resultado['aliquota_efetiva'] = aliq_efet
 
-    if anexo == 3:
+        print("Anexo III - Alíquota Efetiva:", aliq_efet)
+        print("Anexo III - Imposto DAS: {:.2f}".format(resultado['imposto_das']))
+
+    # ANEXO II -  INDUSTRIA
+    elif anexo == 2:
+        if rbt12 > 0 and rbt12 <= 180000:
+            aliq_efet = 0.045
+        elif rbt12 > 180000 and rbt12 <= 360000:
+            aliq_efet = (rbt12 * 0.078 -  5940) / rbt12
+        elif rbt12 > 360000 and rbt12 <= 720000:
+            aliq_efet = (rbt12 * 0.10 - 13860) / rbt12
+        elif rbt12 > 720000 and rbt12 <= 1800000:
+            aliq_efet = (rbt12 * 0.112 - 22500) / rbt12
+        elif rbt12 > 1800000 and rbt12 <= 3600000:
+            aliq_efet = (rbt12 * 0.147 - 85500) / rbt12
+        elif rbt12 > 3600000 and rbt12 <= 4800000:
+            calc_iss = ((rbt12 * 0.21 - 125640) / rbt12) * 0.335
+            iss_excedente = calc_iss * receita_mensal
+            aliq_efet = ((rbt12 * 0.33 - 648000) / rbt12) + calc_iss
+        resultado['imposto_das'] = receita_mensal * aliq_efet
+        resultado['aliquota_efetiva'] = aliq_efet
+
+        print("Anexo III - Alíquota Efetiva:", aliq_efet)
+        print("Anexo III - Imposto DAS: {:.2f}".format(resultado['imposto_das']))
+
+
+    elif anexo == 3:
         if rbt12 > 0 and rbt12 <= 180000:
             aliq_efet = 0.06
         elif rbt12 > 180000 and rbt12 <= 360000:
